456f97b13fc53bcd5ddd76ccec046c6e1efac753
PokeFred <Cedric.paelmke@gmx.de>
2024-01-20 23:43:49 +0100

    configure SkeletonUI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
     import '../app.css'
+    import 'highlight.js/styles/github-dark.css'
+    import { initializeStores, Drawer, Modal, Toast, storePopup, storeHighlightJs } from '@skeletonlabs/skeleton'
+    import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom'
+    import hljs from 'highlight.js/lib/core'
+
+    async function loadHighlights(): Promise<void> {
+        hljs.registerLanguage('javascript', (await import('highlight.js/lib/languages/javascript')).default)
+        hljs.registerLanguage('typescript', (await import('highlight.js/lib/languages/typescript')).default)
+        hljs.registerLanguage('json', (await import('highlight.js/lib/languages/json')).default)
+    }
+
+    initializeStores()
+    storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow })
+    loadHighlights()
+    storeHighlightJs.set(hljs)
 </script>
 
 <div data-theme="skeleton" class="dark">
+    <!-- Skeleton Utility initializes -->
+    <Drawer />
+    <Modal />
+    <Toast />
+
+    <!-- the actual page -->
     <slot />
 </div>
