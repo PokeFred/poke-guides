8f8192c80a3b69ba24bd6c5b7c6a21d1f818f49f
PokeFred <Cedric.paelmke@gmx.de>
2024-07-23 16:42:41 +0200

    add tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,7 @@
     import { faSmile } from "@fortawesome/free-solid-svg-icons/faSmile"
 </script>
 
-<h1>Welcome to SvelteKit <Icon data={faSmile} /></h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="p-4">
+    <h1 class="text-lg underline">Welcome to SvelteKit <Icon data={faSmile} /></h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
