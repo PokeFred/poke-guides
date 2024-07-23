96b289b11c4a24e86de3cdca79a9d37aba86ae25
PokeFred <Cedric.paelmke@gmx.de>
2024-07-23 16:37:33 +0200

    add icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+    import Icon from "svelte-awesome"
+    import { faSmile } from "@fortawesome/free-solid-svg-icons/faSmile"
+</script>
+
+<h1>Welcome to SvelteKit <Icon data={faSmile} /></h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
