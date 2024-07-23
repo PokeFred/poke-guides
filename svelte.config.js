8c042c8568c3d20d97a33de68daaa8de0daf1e7f
PokeFred <Cedric.paelmke@gmx.de>
2024-07-23 15:17:06 +0200

    migrate from node to vercel servers

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,11 @@
-import adapter from "@sveltejs/adapter-node"
+import adapter from "@sveltejs/adapter-vercel"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
     kit: {
-        adapter: adapter({
-            out: "./build",
-            precompress: true,
-            envPrefix: ""
-        }),
+        adapter: adapter(),
         env: {
             publicPrefix: "PUBLIC_",
             privatePrefix: "PRIVATE_"
